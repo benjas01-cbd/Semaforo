10ec84dbb778662b0b2d4af8910095dc5a5e1253
Vespertino <Vespertino@DESKTOP-TGPE4MD.localdomain>
2021-05-07 00:28:25 +0200

    Primera version

---
+++ src/Clases/Profesor.java
@@ -0,0 +1,14 @@
+package clases;
+
+public class Profesor  extends Persona{
+    	public Profesor() {}				// Constructor
+
+	// Hace la pregunta al estudiante sobre el color
+    	public String preguntacolor(){
+
+		Estudiante alumno;
+                alumno = new Estudiante();
+		String colorRec = alumno.preguntacolor();
+		return colorRec;
+    	}
+}
